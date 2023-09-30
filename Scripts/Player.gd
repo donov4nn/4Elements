23ee7fafc88b587415f73b63adbb227798c4aaf9
donov4nn <dono.m51@gmail.com>
2023-09-30 16:54:18 +0200

    Added script for player / rename

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,43 +1,27 @@
 extends CharacterBody2D
 
 
-@export var MAX_SPEED = 300
-@export var ACCELERATION = 1500
-@export var FRICTION = 1200
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-@onready var axis = Vector2.ZERO
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	move(delta)
-	
-func get_input_axis():
-	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
-	return axis.normalized()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-func move(delta):
-	axis = get_input_axis()
-	
-	if axis == Vector2.ZERO:
-		apply_friction(FRICTION * delta)
-		
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		apply_movement(axis * ACCELERATION * delta	)
-	
-	move_and_slide() 
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-func apply_friction(amount):
-	if velocity.length() > amount:
-		velocity -= velocity.normalized() * amount
-	
-	else: 
-		velocity = Vector2.ZERO
-		
-func apply_movement(accel):
-	velocity += accel
-	velocity = velocity.limit_length(MAX_SPEED)
-	
-	
-
-	
-	
+	move_and_slide()
